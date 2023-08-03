c982989f3ba0afb72facd6d826e1b224b563d8c6
AlexIn.Tech <18593496+AlexIn-Tech@users.noreply.github.com>
2023-08-03 08:43:43 +0200

    Add files via upload

---
+++ hourly-wage-calculator.ps1
@@ -0,0 +1,82 @@
+﻿# Load necessary assembly for creating form
+Add-Type -AssemblyName System.Windows.Forms
+
+# Function to convert input string to double
+function ConvertTo-Double {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$inputString
+    )
+    
+    $culture = [System.Globalization.CultureInfo]::InvariantCulture
+    if ($inputString.Contains(",")) {
+        $culture = [System.Globalization.CultureInfo]::CreateSpecificCulture('de-DE')
+    }
+    return [double]::Parse($inputString, $culture)
+}
+
+# Create form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Hourly Wage Calculator'
+$form.Size = New-Object System.Drawing.Size(300,250)
+$form.StartPosition = 'CenterScreen'
+
+# Create salary label and textbox
+$grossSalaryLabel = New-Object System.Windows.Forms.Label
+$grossSalaryLabel.Location = New-Object System.Drawing.Point(10,20)
+$grossSalaryLabel.Size = New-Object System.Drawing.Size(280,20)
+$grossSalaryLabel.Text = 'Enter your total gross monthly salary :'
+$form.Controls.Add($grossSalaryLabel)
+
+$grossSalaryTextBox = New-Object System.Windows.Forms.TextBox
+$grossSalaryTextBox.Location = New-Object System.Drawing.Point(10,40)
+$grossSalaryTextBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($grossSalaryTextBox)
+
+# Create net salary label and textbox
+$netSalaryLabel = New-Object System.Windows.Forms.Label
+$netSalaryLabel.Location = New-Object System.Drawing.Point(10,70)
+$netSalaryLabel.Size = New-Object System.Drawing.Size(280,20)
+$netSalaryLabel.Text = 'Enter your total net monthly salary :'
+$form.Controls.Add($netSalaryLabel)
+
+$netSalaryTextBox = New-Object System.Windows.Forms.TextBox
+$netSalaryTextBox.Location = New-Object System.Drawing.Point(10,90)
+$netSalaryTextBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($netSalaryTextBox)
+
+# Create hours label and textbox
+$hoursLabel = New-Object System.Windows.Forms.Label
+$hoursLabel.Location = New-Object System.Drawing.Point(10,120)
+$hoursLabel.Size = New-Object System.Drawing.Size(280,20)
+$hoursLabel.Text = 'Enter the number of hours you work per week:'
+$form.Controls.Add($hoursLabel)
+
+$hoursTextBox = New-Object System.Windows.Forms.TextBox
+$hoursTextBox.Location = New-Object System.Drawing.Point(10,140)
+$hoursTextBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($hoursTextBox)
+
+# Create button for calculation
+$button = New-Object System.Windows.Forms.Button
+$button.Location = New-Object System.Drawing.Point(10,170)
+$button.Size = New-Object System.Drawing.Size(75,23)
+$button.Text = 'Calculate'
+$button.Add_Click({
+    $weeksPerMonth = 4.33 # average number of weeks in a month
+
+    $monthlyGrossSalary = ConvertTo-Double $grossSalaryTextBox.Text
+    $monthlyNetSalary = ConvertTo-Double $netSalaryTextBox.Text
+    $hoursPerWeek = ConvertTo-Double $hoursTextBox.Text
+
+    $hoursPerMonth = $hoursPerWeek * $weeksPerMonth
+
+    $hourlyGrossWage = $monthlyGrossSalary / $hoursPerMonth
+    $hourlyNetWage = $monthlyNetSalary / $hoursPerMonth
+
+    [System.Windows.Forms.MessageBox]::Show("Your gross hourly wage is: CHF " + $hourlyGrossWage.ToString('F2') + "`nYour net hourly wage is: CHF " + $hourlyNetWage.ToString('F2'), "Hourly Wage")
+})
+$form.Controls.Add($button)
+
+# Run the form
+$form.ShowDialog()
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Hourly Wage Calculator (PowerShell)
+
+This PowerShell script provides a GUI for users to calculate their gross and net hourly wage based on their monthly salary and weekly hours worked.
+
+## Prerequisites
+
+- Windows operating system
+- PowerShell 5.1 or higher
+
+## Usage
+
+1. Clone this repository to your local machine or download the `hourly-wage-calculator.ps1` file.
+2. Open PowerShell.
+3. Navigate to the directory containing the `hourly-wage-calculator.ps1` file.
+4. Run the script with the following command: `.\hourly-wage-calculator.ps1`.
+5. In the GUI, enter your total gross monthly salary, net monthly salary, and the number of hours you work per week. Please note, you can use both a comma and a period as the decimal separator.
+6. Click the "Calculate" button. The script will then display your gross and net hourly wages.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+MIT
